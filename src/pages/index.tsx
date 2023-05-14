f625ef591f3786df0fe32ab4a1db72f01183909a
Jafin Jahfar <jafinjahfar@gmail.com>
2023-05-14 22:08:18 +0530

    created separate layout for metrics display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 // import { connectFunctionsEmulator } from 'firebase/functions';
 import React, { useEffect, useState } from 'react';
 // import { functions } from 'config/firebase';
 import Navbar from '@/components/Navbar';
 import FeatureList from '@/layouts/FeatureList';
+import MetricsList from '@/layouts/MetricsList';
 
 function Home() {
   // connectFunctionsEmulator(functions, 'localhost', 5001);
@@ -76,34 +76,7 @@ function Home() {
       </section>
 
       <section className="w-full py-28 px-32 text-gray-300 font-poppins">
-        <hr className="h-px border-0 bg-gradient-to-br from-blue-400 to-purple-500 opacity-60" />
-        <div className="flex flex-col md:flex-row justify-around items-center gap-20 md:gap-0 py-20">
-          <div className="flex flex-col gap-3 justify-center items-center">
-            <p className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-grad-green">
-              2000+
-            </p>
-            <p className="text-xl font-medium">DOWNLOADS</p>
-          </div>
-          <div className="flex flex-col gap-3 justify-center items-center">
-            <p className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-grad-green">
-              1000+
-            </p>
-            <p className="text-xl font-medium">ACTIVE USERS</p>
-          </div>
-          <div className="flex flex-col gap-3 justify-center items-center">
-            <p className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-grad-green">
-              100K+
-            </p>
-            <p className="text-xl font-medium">QUESTIONS ANSWERED</p>
-          </div>
-          <div className="flex flex-col gap-3 justify-center items-center">
-            <p className="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-purple-400 to-grad-green">
-              800+
-            </p>
-            <p className="text-xl font-medium">PREMIUM USERS</p>
-          </div>
-        </div>
-        <hr className="h-px border-0 bg-gradient-to-br from-blue-400 to-purple-500 opacity-60" />
+        <MetricsList />
       </section>
 
       <section className="px-10 w-full text-gray-300 py-10">
