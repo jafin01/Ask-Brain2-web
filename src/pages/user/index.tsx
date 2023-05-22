bcc7e45273a0295a476fff3c27175ec4a5bf0801
Jafin Jahfar <jafinjahfar@gmail.com>
2023-05-22 12:36:23 +0530

    create character completed

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CharecterForm from '@/components/Charecter';
+
+export default function UserProfile() {
+  return (
+    <div>
+      <CharecterForm />
+    </div>
+  );
+}
