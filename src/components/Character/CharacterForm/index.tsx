9754273d4be61b0a17cc0da360107c6fb36a1c18
miquel <jaumepuigturon4@gmail.com>
2023-05-24 20:22:43 +0200

    Fix build issue

--- src/components/Character/CharacterForm/index.tsx
+++ src/components/Character/CharacterForm/index.tsx
@@ -7,14 +7,14 @@ import {
   doc,
   getDoc,
   updateDoc,
-} from "@firebase/firestore";
-import { auth, db, storage } from "config/firebase";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { ErrorMessage, Field, FieldArray, Form, Formik } from "formik";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
-import * as Yup from "yup";
+} from '@firebase/firestore';
+import { auth, db, storage } from 'config/firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { ErrorMessage, Field, FieldArray, Form, Formik } from 'formik';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import * as Yup from 'yup';
 
 function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -22,9 +22,9 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [initialValues, setInitialValues] = useState<any>({
-    name: "",
-    avatar: "",
-    prompts: [{ role: "system", content: "" }],
+    name: '',
+    avatar: '',
+    prompts: [{ role: 'system', content: '' }],
   });
   const router = useRouter();
   const { push } = router;
@@ -33,7 +33,7 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
     const { id } = router.query;
 
     try {
-      const docRef = doc(db, "characters", id as string);
+      const docRef = doc(db, 'characters', id as string);
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         const { name, prompts } = docSnap.data()!;
@@ -47,7 +47,7 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
         setAvatar(docSnap.data().avatar);
       }
     } catch (error) {
-      console.log("Error getting document:", error);
+      console.log('Error getting document:', error);
     }
   }
 
@@ -63,8 +63,8 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
     };
 
     handleResize(); // Check on initial render
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   const handleSubmit = async (values: any, { setSubmitting }: any) => {
@@ -76,23 +76,23 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
       const downloadedAvatarUrl = await getDownloadURL(uploadAvatar);
 
       if (!isUpdate) {
-        await addDoc(collection(db, "characters"), {
+        await addDoc(collection(db, 'characters'), {
           userId: auth.currentUser?.uid,
           name: values.name,
           avatar: downloadedAvatarUrl,
           prompts: values.prompts,
         });
       } else {
-        updateDoc(doc(db, "characters", router.query.id as string), {
+        updateDoc(doc(db, 'characters', router.query.id as string), {
           name: values.name,
           avatar: downloadedAvatarUrl,
           prompts: values.prompts,
         });
       }
 
-      push("/user");
+      push('/user');
     } catch (error) {
-      console.error("Error adding document: ", error);
+      console.error('Error adding document: ', error);
     } finally {
       setSubmitting(false);
       setIsLoading(false);
@@ -102,16 +102,16 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
   const handleAvatarUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (isUpdate) setIsEditing(true);
     const file = e.target.files?.[0];
-    console.log("file", file);
+    console.log('file', file);
     setAvatar(file);
   };
 
   const validationSchema = Yup.object().shape({
-    name: Yup.string().required("Name is required"),
+    name: Yup.string().required('Name is required'),
     prompts: Yup.array().of(
       Yup.object().shape({
-        role: Yup.string().required("Role is required"),
-        content: Yup.string().required("Content is required"),
+        role: Yup.string().required('Role is required'),
+        content: Yup.string().required('Content is required'),
       })
     ),
   });
@@ -120,7 +120,7 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
     <div className="bg-gradient-to-br from-app-bg via-app-bg to-grad-purple min-h-screen flex justify-center items-center">
       <div className="w-full max-w-md p-6 bg-app-bg text-gray-600 rounded-2xl">
         <h1 className="text-4xl font-bold text-transparent bg-gradient-to-r from-grad-green to-white bg-clip-text text-center mb-8">
-          {!isUpdate ? "Create your character" : "Update your character"}
+          {!isUpdate ? 'Create your character' : 'Update your character'}
         </h1>
 
         <Formik
@@ -219,68 +219,69 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
               </label>
               {/* Prompt field array */}
               <FieldArray name="prompts">
-                {({ push, remove }) => (
+                {({ push: innerPush, remove }) => (
                   <div className="flex flex-col gap-2">
-                    {formikProps.values?.prompts?.map((_, index) => (
-                      <div key={index}>
-                        <div
-                          key={index}
-                          className={`flex ${
-                            isMobile ? "flex-col" : "flex-row"
-                          } gap-3 justify-end ${
-                            isMobile ? "items-stretch" : "items-start"
-                          }`}
-                        >
-                          <Field
-                            as="select"
-                            name={`prompts.${index}.role`}
-                            id={`prompts.${index}.role`}
-                            className="p-2 border border-grad-green rounded flex-grow"
+                    {formikProps.values?.prompts?.map(
+                      (_: unknown, index: number) => (
+                        <div>
+                          <div
+                            className={`flex ${
+                              isMobile ? 'flex-col' : 'flex-row'
+                            } gap-3 justify-end ${
+                              isMobile ? 'items-stretch' : 'items-start'
+                            }`}
                           >
-                            <option value="system">
-                              Character description
-                            </option>
-                            <option value="user">User</option>
-                            <option value="assistant">Character</option>
-                          </Field>
-                          <Field
-                            as="textarea"
-                            name={`prompts.${index}.content`}
-                            id={`prompts.${index}.content`}
-                            rows={3}
-                            placeholder="Enter content"
-                            className="p-2 border border-grad-green rounded flex-grow"
-                          />
-                          <button type="button" onClick={() => remove(index)}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="h-6 w-6 text-red-500 hover:text-red-600"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
+                            <Field
+                              as="select"
+                              name={`prompts.${index}.role`}
+                              id={`prompts.${index}.role`}
+                              className="p-2 border border-grad-green rounded flex-grow"
                             >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2.5"
-                                d="M6 18L18 6M6 6l12 12"
-                              />
-                            </svg>
-                          </button>
+                              <option value="system">
+                                Character description
+                              </option>
+                              <option value="user">User</option>
+                              <option value="assistant">Character</option>
+                            </Field>
+                            <Field
+                              as="textarea"
+                              name={`prompts.${index}.content`}
+                              id={`prompts.${index}.content`}
+                              rows={3}
+                              placeholder="Enter content"
+                              className="p-2 border border-grad-green rounded flex-grow"
+                            />
+                            <button type="button" onClick={() => remove(index)}>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6 text-red-500 hover:text-red-600"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2.5"
+                                  d="M6 18L18 6M6 6l12 12"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                          <div className="flex flex-col gap-2 items-center justify-center">
+                            <ErrorMessage
+                              name={`prompts.${index}.content`}
+                              component="span"
+                              //  center error message
+                              className="text-red-500 m-4"
+                            />
+                          </div>
                         </div>
-                        <div className="flex flex-col gap-2 items-center justify-center">
-                          <ErrorMessage
-                            name={`prompts.${index}.content`}
-                            component="span"
-                            //  center error message
-                            className="text-red-500 m-4"
-                          />
-                        </div>
-                      </div>
-                    ))}
+                      )
+                    )}
                     <button
                       type="button"
-                      onClick={() => push({ role: "", content: "" })}
+                      onClick={() => innerPush({ role: '', content: '' })}
                       className="flex flex-row gap-2 items-center border border-grad-green rounded p-2 items-center justify-center"
                     >
                       Add Prompt
@@ -312,7 +313,7 @@ function CharacterForm({ isUpdate }: { isUpdate: boolean }) {
                   disabled={isLoading}
                   className="bg-grad-purple hover:bg-grad-purple-dark text-white font-bold py-2 px-4 rounded"
                 >
-                  {isLoading ? "Saving..." : "Save"}
+                  {isLoading ? 'Saving...' : 'Save'}
                 </button>
               </div>
             </Form>
