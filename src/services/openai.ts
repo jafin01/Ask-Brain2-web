2b9ef8a748d9f6af6a2586c7488780d6c8cf4f82
miquel <jaumepuigturon4@gmail.com>
2023-05-26 09:47:43 +0200

    Update to use functions

--- src/services/openai.ts
+++ src/services/openai.ts
@@ -1,27 +1,18 @@
-const apiKey = 'YOUR_API_KEY';
-const apiUrl = 'https://api.openai.com/v1/chat/completions';
+import { functions } from "config/firebase";
+import { httpsCallable } from "firebase/functions";
 
 const sendMessage = async (messages: { content: string; role: string }[]) => {
   try {
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
-        messages: messages.filter((message) => message.content),
-      }),
+    const generateChat = httpsCallable(functions, "generateChat");
+
+    const response: any = await generateChat({
+      model: "gpt-3.5-turbo",
+      prompt: messages.filter((message) => message.content),
     });
 
-    const data = await response.json();
-
-    const reply = data.choices[0].message.content;
-
-    return reply;
+    return response.data.choices[0].message.content;
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
   return null;
 };
