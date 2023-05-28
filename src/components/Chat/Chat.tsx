6ed7f26aa60feaee0b157742d2fb7530f5c02927
miquel <jaumepuigturon4@gmail.com>
2023-05-28 16:30:43 +0200

    Add click events

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,19 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Lottie from 'react-lottie';
-import sendMessage from '@/services/openai';
-import loadingData from '../../../public/assets/loading-dots.json';
+import React, { useEffect, useRef, useState } from "react";
+import Lottie from "react-lottie";
+import sendMessage from "@/services/openai";
+import loadingData from "../../../public/assets/loading-dots.json";
+import { logEvent } from "firebase/analytics";
+import { analytics } from "config/firebase";
 
 const MAX_MESSAGE_COUNT = 100;
 
 function Chat({
-  firstMessage = '',
+  firstMessage = "",
   prompts,
 }: {
   firstMessage: string;
   prompts: { content: string; role: string }[];
 }) {
   const chatRef = useRef<HTMLDivElement>(null);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [conversation, setConversation] =
     useState<{ content: string; role: string; loading?: boolean }[]>(prompts);
 
@@ -23,21 +25,21 @@ function Chat({
     setResponseFinished(false);
     setConversation((prevConversation = []) => [
       ...prevConversation,
-      { content: message, role: 'user' },
-      { loading: true, role: '', content: '' },
+      { content: message, role: "user" },
+      { loading: true, role: "", content: "" },
     ]);
-    setMessage('');
+    setMessage("");
 
     const messagesToSend = [
       ...prompts,
       ...conversation,
-      { content: message, role: 'user' },
+      { content: message, role: "user" },
     ];
     const response = await sendMessage(messagesToSend);
 
     setConversation((prevConversation = []) => [
       ...prevConversation.slice(0, -1),
-      { content: response, role: 'assistant' },
+      { content: response, role: "assistant" },
     ]);
     setResponseFinished(true);
   };
@@ -45,7 +47,7 @@ function Chat({
   useEffect(() => {
     chatRef.current?.scrollTo({
       top: chatRef.current?.scrollHeight,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   }, [conversation]);
 
@@ -58,15 +60,15 @@ function Chat({
         className="absolute right-0 left-0 bottom-16 px-4 pb-4 top-0 pt-4 overflow-scroll flex flex-col"
       >
         <div className="mt-auto flex flex-col gap-1 justify-end">
-          {[{ content: firstMessage, role: 'assistant' }, ...conversation]
+          {[{ content: firstMessage, role: "assistant" }, ...conversation]
             ?.filter((m) => m.content || m.loading)
             ?.map((m) => (
               <div
                 className={`${
-                  m.role === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-200'
-                } ${m.loading ? 'p-0' : 'p-2.5'} rounded-2xl ${
-                  m.role === 'user' ? 'rounded-br-none' : 'rounded-bl-none'
-                } ${m.role === 'user' ? 'self-end' : 'self-start'}`}
+                  m.role === "user" ? "bg-blue-500 text-white" : "bg-gray-200"
+                } ${m.loading ? "p-0" : "p-2.5"} rounded-2xl ${
+                  m.role === "user" ? "rounded-br-none" : "rounded-bl-none"
+                } ${m.role === "user" ? "self-end" : "self-start"}`}
               >
                 {m.loading ? (
                   <Lottie
@@ -96,6 +98,14 @@ function Chat({
                     target="_blank"
                     className="underline"
                     rel="noreferrer"
+                    onClick={async () => {
+                      const resolvedAnalytics = await analytics;
+                      if (resolvedAnalytics !== null) {
+                        logEvent(resolvedAnalytics, "app_click", {
+                          app: "ios",
+                        });
+                      }
+                    }}
                   >
                     <img
                       alt="app store"
@@ -108,6 +118,14 @@ function Chat({
                     target="_blank"
                     className="underline"
                     rel="noreferrer"
+                    onClick={async () => {
+                      const resolvedAnalytics = await analytics;
+                      if (resolvedAnalytics !== null) {
+                        logEvent(resolvedAnalytics, "app_click", {
+                          app: "android",
+                        });
+                      }
+                    }}
                   >
                     <img
                       alt="play store"
@@ -125,7 +143,7 @@ function Chat({
         <input
           onKeyDown={(e) => {
             if (
-              e.key === 'Enter' &&
+              e.key === "Enter" &&
               !e.shiftKey &&
               message &&
               responseFinished &&
@@ -149,8 +167,8 @@ function Chat({
           className={`${
             message &&
             (!responseFinished || conversation.length >= MAX_MESSAGE_COUNT)
-              ? 'opacity-50'
-              : ''
+              ? "opacity-50"
+              : ""
           } absolute top-3 right-4`}
         >
           <svg
