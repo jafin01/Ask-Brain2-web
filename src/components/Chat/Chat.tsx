6da0ab381548f78069c9df38582af809b9d708c8
chandrashekar <kusumachandu43@gmail.com>
2023-06-01 17:33:22 +0530

    resolved conflicts

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useRef, useState } from 'react';
 import Lottie from 'react-lottie';
 import { auth, db } from 'config/firebase';
@@ -6,23 +5,15 @@ import { addDoc, collection, doc, updateDoc } from '@firebase/firestore';
 import loadingData from '../../../public/assets/loading-dots.json';
 import sendMessage from '@/services/openai';
 import Button from '../Button';
-=======
-import React, { useEffect, useRef, useState } from "react";
-import Lottie from "react-lottie";
-import { auth, db } from "config/firebase";
-import { addDoc, collection, doc, updateDoc } from "@firebase/firestore";
-import loadingData from "../../../public/assets/loading-dots.json";
-import sendMessage from "@/services/openai";
->>>>>>> 95c3cf9 (Add user uid)
 
 const MAX_MESSAGE_COUNT = 10;
 
 function Chat({
-  id = "",
-  firstMessage = "",
+  id = '',
+  firstMessage = '',
   prompts,
   judge,
-  characterName = "Assistant",
+  characterName = 'Assistant',
 }: {
   firstMessage: string;
   characterName: string;
@@ -31,27 +22,27 @@ function Chat({
   id?: string;
 }) {
   const chatRef = useRef<HTMLDivElement>(null);
-  const [message, setMessage] = useState("");
-  const [conversationId, setConversationId] = useState("");
+  const [message, setMessage] = useState('');
+  const [conversationId, setConversationId] = useState('');
   const [conversation, setConversation] =
     useState<{ content: string; role: string; loading?: boolean }[]>(prompts);
 
   const [responseFinished, setResponseFinished] = useState(true);
   const [challengeCompleted, setChallengeCompleted] = useState(false);
 
-  const [userUid, setUserUid] = useState("");
+  const [userUid, setUserUid] = useState('');
 
   useEffect(() => {
     if (auth.currentUser?.uid) {
       setUserUid(auth.currentUser?.uid);
     } else {
-      const userUid = localStorage.getItem("userUid");
-      if (userUid) {
-        setUserUid(userUid);
+      const userId = localStorage.getItem('userUid');
+      if (userId) {
+        setUserUid(userId);
       } else {
         const tmpUid = Math.random().toString(36).substring(7);
-        localStorage.setItem("userUid", tmpUid);
-        setUserUid(localStorage.getItem("userUid") || "");
+        localStorage.setItem('userUid', tmpUid);
+        setUserUid(localStorage.getItem('userUid') || '');
       }
     }
   }, [auth.currentUser?.uid]);
@@ -60,28 +51,28 @@ function Chat({
     setResponseFinished(false);
     setConversation((prevConversation = []) => [
       ...prevConversation,
-      { content: message, role: "user" },
-      { loading: true, role: "", content: "" },
+      { content: message, role: 'user' },
+      { loading: true, role: '', content: '' },
     ]);
-    setMessage("");
+    setMessage('');
 
     const messagesToSend = [
       ...prompts,
       ...conversation,
-      { content: message, role: "user" },
+      { content: message, role: 'user' },
     ];
     const response = await sendMessage(messagesToSend);
 
     setConversation((prevConversation = []) => [
       ...prevConversation.slice(0, -1),
-      { content: response, role: "assistant" },
+      { content: response, role: 'assistant' },
     ]);
     setResponseFinished(true);
   };
 
   useEffect(() => {
     const update = async () => {
-      if (judge && conversation?.slice(-1)?.[0]?.role === "assistant") {
+      if (judge && conversation?.slice(-1)?.[0]?.role === 'assistant') {
         // get last judge.numMessages messages
         const judgeMessages = conversation.slice(-judge.numMessages);
 
@@ -94,14 +85,14 @@ function Chat({
         Messages:
         ${judgeMessages
           .map((m) =>
-            m.role === "user" ? "User" : `${characterName}: ${m.content}`
+            m.role === 'user' ? 'User' : `${characterName}: ${m.content}`
           )
-          .join("\n")}`,
-            role: "user",
+          .join('\n')}`,
+            role: 'user',
           },
         ]);
 
-        if (judgeResponse.toLowerCase().includes("true")) {
+        if (judgeResponse.toLowerCase().includes('true')) {
           setChallengeCompleted(true);
         }
       }
@@ -113,31 +104,27 @@ function Chat({
     const update = async () => {
       chatRef.current?.scrollTo({
         top: chatRef.current?.scrollHeight,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
       try {
         if (!conversationId) {
-          await addDoc(collection(db, "chats"), {
+          await addDoc(collection(db, 'chats'), {
             messages: [],
             user_id: userUid,
             createdAt: new Date().toISOString(),
-            title: "Web ask Brain2 Conversation",
+            title: 'Web ask Brain2 Conversation',
             characterId: id,
           }).then((docRef) => {
             setConversationId(docRef.id);
           });
         } else {
-          await updateDoc(doc(db, "chats", conversationId), {
+          await updateDoc(doc(db, 'chats', conversationId), {
             messages: conversation,
             challengeCompleted,
           });
         }
       } catch (error) {
-<<<<<<< HEAD
-        console.log('Error adding document: ', error);
-=======
-        console.error("Error adding document: ", error);
->>>>>>> 95c3cf9 (Add user uid)
+        console.error('Error adding document: ', error);
       }
     };
 
@@ -153,15 +140,15 @@ function Chat({
         className="absolute right-0 left-0 bottom-16 px-4 pb-4 top-0 pt-4 overflow-scroll flex flex-col"
       >
         <div className="mt-auto flex flex-col gap-1 justify-end">
-          {[{ content: firstMessage, role: "assistant" }, ...conversation]
+          {[{ content: firstMessage, role: 'assistant' }, ...conversation]
             ?.filter((m) => m.content || m.loading)
             ?.map((m) => (
               <div
                 className={`${
-                  m.role === "user" ? "bg-blue-500 text-white" : "bg-gray-200"
-                } ${m.loading ? "p-0" : "p-2.5"} rounded-2xl ${
-                  m.role === "user" ? "rounded-br-none" : "rounded-bl-none"
-                } ${m.role === "user" ? "self-end" : "self-start"}`}
+                  m.role === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-200'
+                } ${m.loading ? 'p-0' : 'p-2.5'} rounded-2xl ${
+                  m.role === 'user' ? 'rounded-br-none' : 'rounded-bl-none'
+                } ${m.role === 'user' ? 'self-end' : 'self-start'}`}
               >
                 {m.loading ? (
                   <Lottie
@@ -197,8 +184,8 @@ function Chat({
                       rel="noreferrer"
                       onClick={async () => {
                         try {
-                          await addDoc(collection(db, "clicks"), {
-                            app: "ios",
+                          await addDoc(collection(db, 'clicks'), {
+                            app: 'ios',
                             user_id: userUid,
                             createdAt: new Date().toISOString(),
                             characterId: id,
@@ -221,8 +208,8 @@ function Chat({
                       rel="noreferrer"
                       onClick={async () => {
                         try {
-                          await addDoc(collection(db, "clicks"), {
-                            app: "android",
+                          await addDoc(collection(db, 'clicks'), {
+                            app: 'android',
                             user_id: userUid,
                             createdAt: new Date().toISOString(),
                             characterId: id,
@@ -249,7 +236,7 @@ function Chat({
         <input
           onKeyDown={(e) => {
             if (
-              e.key === "Enter" &&
+              e.key === 'Enter' &&
               !e.shiftKey &&
               message &&
               responseFinished &&
@@ -277,8 +264,8 @@ function Chat({
             (!responseFinished ||
               conversation.length >= MAX_MESSAGE_COUNT ||
               challengeCompleted)
-              ? "opacity-50"
-              : ""
+              ? 'opacity-50'
+              : ''
           } absolute top-3 right-4`}
         >
           <svg
@@ -305,6 +292,6 @@ function Chat({
 export default Chat;
 
 Chat.defaultProps = {
-  id: "",
+  id: '',
   judge: null,
 };
